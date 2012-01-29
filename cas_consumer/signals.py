6fca1d7bed731ebb8d1279bcb09657e0db34e0d6
deyk <deyk@crossway.org>
2012-01-28 17:30:56 -0800

    Made the signal import a little nicer.

--- cas_consumer/signals.py
+++ cas_consumer/signals.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 """cas_consumer.signals -- signal definitions for the CAS client.
 """
-import django.dispatch
+from django import dispatch
 
 
 # Sent when multiple users match the CAS verification response. The
 # primary user will be used for authentication. The others may be
 # merged into the primary as appropriate for the application.
-on_cas_merge_users = django.dispatch.Signal(providing_args=["primary", "others"])
+on_cas_merge_users = dispatch.Signal(providing_args=["primary", "others"])
 
 
 # Sent when a user has been successfully authenticated by CAS.
-on_cas_authentication = django.dispatch.Signal(providing_args=['user'])
+on_cas_authentication = dispatch.Signal(providing_args=['user'])
