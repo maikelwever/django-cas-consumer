01d5fe49eb049ac5164e978be549038a0bad969b
deyk <deyk@crossway.org>
2012-06-21 11:28:45 -0700

    Fixed bug where merge signal log entry was stating the wrong list of other users to be merged. No change in a actual behavior.
    Bumped version to 0.3.5.1

--- cas_consumer/backends.py
+++ cas_consumer/backends.py
@@ -226,10 +226,11 @@ class CASBackend(object):
             user.save()
 
         if len(users) > 1:
-            logger.info('Sending merge signal for other users: %s', users[1:])
+            others = [u for u in users if u.username != user.username]
+            logger.info('Sending merge signal for other users: %s', others)
             try:
                 result = signals.on_cas_merge_users.send(sender=self, primary=user,
-                                                         others=[u for u in users if u.username != user.username])
+                                                         others=others)
             except Exception:
                 logger.exception('Merge signal failed!')
             else:
