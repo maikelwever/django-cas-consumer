57141bcdd628eae33656ea95cfb8e1576f192332
Maikel Wever <maikelwever@gmail.com>
2012-12-09 01:07:11 +0100

    See if this makes any difference...

--- cas_consumer/urls.py
+++ cas_consumer/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls.defaults import *
 from cas_consumer.views import *
 
 urlpatterns = patterns('',
-    url(r'^login/', login, name="cas_login"),
-    url(r'^logout/', logout, name="cas_logout"),
+    url(r'login/', login, name="cas_login"),
+    url(r'logout/', logout, name="cas_logout"),
 )
